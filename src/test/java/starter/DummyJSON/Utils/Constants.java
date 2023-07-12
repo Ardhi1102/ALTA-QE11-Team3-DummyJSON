2481f231d10d7c5106ba8f7dfe9acc65b8854d77
Ardhi Wiranata <Awiranata228@gmail.com>
2023-07-12 23:43:21 +0700

    updateCarts

---
+++ src/test/java/starter/DummyJSON/Utils/Constants.java
@@ -0,0 +1,11 @@
+package starter.DummyJSON.Utils;
+
+public class Constants {
+    public static String BASE_URL = "https://dummyjson.com";
+    public static final String DIR = System.getProperty("user.dir");
+    public static String JSON = DIR + "/src/test/resources/JSON";
+    public static String JSON_Schema = JSON + "/JSONSchema";
+    public static String JSON_Body = JSON + "/JSONBody";
+    public static String Carts = JSON_Schema + "/Carts";
+
+}
