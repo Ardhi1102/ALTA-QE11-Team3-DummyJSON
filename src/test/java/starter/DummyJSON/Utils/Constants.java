7151f40c760c9fefe5c96352803ca06a4a075b4b
Ardhi Wiranata <Awiranata228@gmail.com>
2023-07-13 10:28:52 +0700

    updateComment

---
+++ src/test/java/starter/DummyJSON/Utils/Constants.java
@@ -0,0 +1,14 @@
+package starter.DummyJSON.API.Utils;
+
+public class Constants {
+
+        public static String BASE_URL = "https://dummyjson.com";
+        public static final String DIR = System.getProperty("user.dir");
+        public static String JSON = DIR + "/src/test/resources/JSON";
+        public static String JSON_Schema = JSON + "/JSONSchema";
+        public static String JSON_Body = JSON + "/JSONBody";
+        public static String Auth_Body = JSON_Body + "/Auth";
+        public static String Auth_Schema = JSON_Schema + "/Auth";
+
+    }
+}
