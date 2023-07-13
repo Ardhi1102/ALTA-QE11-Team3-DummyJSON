e8e5d9458105613d7994c096ca1f373be679b1a1
sultanjoss <fahmim.khairul15@gmail.com>
2023-07-14 02:00:02 +0700

    Alhamdulillah

---
+++ src/test/java/DummyJSON/DummyJSONResponses/UsersResponse.java
@@ -0,0 +1,11 @@
+package DummyJSON.DummyJSONResponses;
+
+public class UsersResponse {
+    public static String ID = "id";
+    public static String fisrtName = "firstName";
+    public static String lastName = "lastName";
+    public static String maidenName = "maidenName";
+
+    public static String isDeleted = "isDeleted";
+
+}
